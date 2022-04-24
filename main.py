0f74ec0add282a8f725317cc5fd0553103be361b
rockstarcuba <102777282+rockstarcuba@users.noreply.github.com>
2022-04-24 17:21:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -477,10 +477,7 @@ def onmessage(update,bot:ObigramClient):
         thread.store('msg',message)
 
         if '/start' in msgText:
-            start_msg = 'Bot          : TGUploaderPro v7.0\n'
-            start_msg+= 'Desarrollador: @obisoftdev\n'
-            start_msg+= 'Api          : https://github.com/Obysoftt/pyobigram\n'
-            start_msg+= 'Uso          :Envia Enlaces De Descarga y Archivos Para Procesar (Configure Antes De Empezar , Vea El /tutorial)\n'
+            start_msg = 'Bot de descargas y subidas a la nube☁️ en su versión 7.1🤖. Disfrute de este bot 👨‍💻\n'
             bot.editMessageText(message,start_msg)
         elif '/files' == msgText and user_info['cloudtype']=='moodle':
              proxy = ProxyCloud.parse(user_info['proxy'])
